018b0b4414d34703a2d6c0cb3d92ed4b273ea8fa
JasperKrol <jasper.paul.krol@gmail.com>
2021-06-06 17:42:08 +0200

    start form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -5,23 +5,30 @@ function Contact() {
         <div className="container">
             <section className="tile1">
 
+                <div className="about-us">
                 <h3>About us:</h3>
-                <div className="user-photo">
-                    <i className="far fa-user"/>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda, cum enim eveniet fuga iusto
+                        odio optio voluptas. At atque iste perferendis quaerat. Aliquid asperiores culpa, eius excepturi
+                        explicabo iste voluptas.</p>
                 </div>
-                <div className="home-text">
-                    <p>View the leaderboards to plan you next trip or training!</p>
+                <div className="company-details">
+                    <i className="fas fa-map-marked-alt"/>
+                    <i className="fas fa-phone-alt"/>
+                    <i className="far fa-comment"/>
                 </div>
+                <a href="https://www.strava.com/"><p className='go-to-strava'>Go to strava<i
+    className="fas fa-arrow-circle-right"/></p></a>
+
 
             </section>
             <section className="tile2">
-                <h3>Contact us:</h3>
-                <div className="user-photo">
-                    <i className="far fa-user"/>
+                <div className="about-us">
+                    <h3>Contact us:</h3>
+                    <label htmlFor="contact-information">Name:
+                        <input type="text"/>
+                    </label>
                 </div>
-                <div className="home-text">
-                    <p>View the leaderboards to plan you next trip or training!</p>
-                </div>
+
             </section>
         </div>
     )
