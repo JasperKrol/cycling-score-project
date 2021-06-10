e7760826facc4b4435ca92bc0edc651d692cbbaf
JasperKrol <jasper.paul.krol@gmail.com>
2021-06-10 16:37:25 +0200

    start with your scores

--- src/pages/Yourscores.js
+++ src/pages/Yourscores.js
@@ -1,6 +1,20 @@
+import "../components/Yourscores.css"
+
 function YourScores () {
     return (
-        <h4>Hoogte snelheid en afstand</h4>
+        <>
+            <div className="container">
+                <section className="tile">
+                    <h3>Welcome "User"!</h3>
+                    <div className="user-photo">
+                        <i className="far fa-user"/>
+                    </div>
+                    <div className="home-text">
+                        <p>View the leaderboards to plan you next trip or training!</p>
+                    </div>
+                </section>
+            </div>
+        </>
     )
 }
 
